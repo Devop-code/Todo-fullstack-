da33f66891706311fb999c452f65b49f423259ea
Devop-code <kamgadevop@gmail.com>
2025-02-13 13:41:27 +0100

    Ajout de la configuration des URL et des migrations initiales pour le modèle CustomUser et Task dans l'API Django; conflit entre les dependances du projet principale

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
 
+# mise en place de la classe personaliser pour gerer nos utilisateurs qui etant de la classe BaseUsermanagement parceque je veux personaliser mon modele unser sinon j'aurais pus utiliser modele user personaliser
+class CustomUserManager(BaseUserManager):
+    # definition de la methode create_user pour creer un utilisateur
+    def create_user(self,username,email,password=None,role='user'):
+        if not username:
+            raise ValueError('user must have an username')
+        if not email:
+            raise ValueError('user must have an email')
+        # normalisation de l'email fournie pas l'utilisateur 
+        email = self.normalize_email(email)
+        user = self.model(username=username,email=email,role=role)
+        # mise a jour du mot de passe user
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    # definition de la methode create_superuser pour creer un super utilisateur
+    def create_superuser(self , username,email,password=None):
+        user=self.create_user(username=username,email=email,password=password,role='admin')
+        user.is_admin=True
+        user.save(using=self._db)
+        return user
+    # definition de la classe CustomUser qui etant de la classe AbstractBaseUser materialisant les utilisateurs de notre application
+class CustomUser(AbstractBaseUser):
+    # le champ password n'existe pas parceque je vais utiliser celle de la classe que mon modle herite et a des outils integrer pour gerer les mot de passe fournis par django
+    username=models.CharField( unique=True,max_length=100)
+    email=models.EmailField(unique=True)
+    role=models.CharField(default='user',max_length=100)
+    is_active=models.BooleanField(default=True)
+    is_admin=models.BooleanField(default=False)
+    # gestionnaire d'objet pour mon modele CustomUser
+    objects=CustomUserManager()
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 # Create your models here.
+# creation du modele pour les taches
+class Task(models.Model):
+    title=models.CharField(max_length=150)
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    # creation du champ qui met un lien entre les utilisateur qt les taches supprimer et qui definir que pour chaque utilisateur supprimer on supprime les taches qui lui sont lie pour eviter d'avoir des donnee orphelin dans la BD
+    author= models.ForeignKey(CustomUser, related_name='tasks', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,9 +25,20 @@ SECRET_KEY = 'django-insecure-9w%k1poopfb7u%h&nyea8cdd)b#@^7yeiacfa#i(ya6&1!j@qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +48,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "api",
+    "rest_framework",
+    "corsheaders",
 ]
+AUTH_USER_MODEL='api.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -121,3 +137,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOW_CREDENTIALS=True
